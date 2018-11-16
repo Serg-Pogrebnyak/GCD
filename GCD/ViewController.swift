54a43b4ac15a41e998b526389ab9231fb10d91f9
Sergey Pogrebnyak <sergey.pogrebnuak@nixsolutions.com>
2018-11-16 17:24:50 +0200

    empty project + git ignore

---
+++ GCD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GCD
+//
+//  Created by Sergey Pogrebnyak on 16.11.2018.
+//  Copyright © 2018 Sergey Pogrebnyak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
